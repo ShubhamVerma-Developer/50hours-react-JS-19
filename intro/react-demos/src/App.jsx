c2491fe0881805552d90e4f0f06b27cf3bb7189d
ShubhamVerma-Developer <shubham25122001@gmail.com>
2025-02-26 21:30:30 +0530

    create the WelcomeMessage component and render this component in the App.jsx

--- intro/react-demos/src/App.jsx
+++ intro/react-demos/src/App.jsx
@@ -1,13 +1,8 @@
-
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 function App() {
   return <div>
-    <Header />
-    <MainContent />
-    <Footer />
+    <WelcomeMessage />
   </div>
 }
 
