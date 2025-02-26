3ee56d156f12e9ca478d580fc374573b871a0793
ShubhamVerma-Developer <shubham25122001@gmail.com>
2025-02-26 21:36:20 +0530

    do not use for in the label use the htmlFor instead of the for

--- intro/react-demos/src/components/WelcomeMessage.jsx
+++ intro/react-demos/src/components/WelcomeMessage.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const WelcomeMessage = () => {
   return (
-    <div>
-        <h1>Hello, World</h1>
-        <p>Welcome to learning JSX!</p>
-    </div>
+    <section id="section">
+        <form>
+            <label htmlFor="name">Name</label>
+            <input type="text" placeholder="Enter your name" />
+        </form>
+
+    </section>
   )
 }
 
