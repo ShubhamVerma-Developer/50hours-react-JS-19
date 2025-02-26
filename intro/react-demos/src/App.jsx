bf76d6721e65a5a099401d4673798909eed0185f
ShubhamVerma-Developer <shubham25122001@gmail.com>
2025-02-26 21:58:07 +0530

    dynamically pass the variable and  function in the jsx

--- intro/react-demos/src/App.jsx
+++ intro/react-demos/src/App.jsx
@@ -1,8 +1,22 @@
-import JSXRules from "./components/JSXRules";
 
 function App() {
+
+  const myName = "my name is John Doe";
+
+  const multiply = (a, b) => a *b;
+
+  const specialClass = "special-class";
+
   return <div>
-    <JSXRules />  
+    <p>
+      {2 + 2}
+    </p>
+    <h1>
+      {myName}
+    </h1>
+    <p>My Friend List: {["Alex", "John", "Waheed", "Jorden"]}</p>
+    <p>2 * 2 = {multiply(2, 2)}</p>
+    <p className={specialClass}>This is special class</p>
   </div>
 }
 
